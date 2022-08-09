8b6117b7e5cbb567a86a2c71823655ccf304093a
MahidarP <110811646+MahidarP@users.noreply.github.com>
2022-08-09 11:17:03 +0530

    Add files via upload

---
+++ Reivew/Class13.cs
@@ -0,0 +1,42 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiple
+{
+    interface IC
+    {
+        void myMethod();
+    }
+
+    interface ICM
+    {
+        void myOtherMethod();
+    }
+
+
+    class DemoClass : IC, ICM
+    {
+        public void myMethod()
+        {
+            Console.WriteLine("This is my method");
+        }
+
+        public void myOtherMethod()
+        {
+            Console.WriteLine("This is first");
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DemoClass myObj = new DemoClass();
+            myObj.myMethod();
+            myObj.myOtherMethod();
+        }
+
+    }
+}*/
